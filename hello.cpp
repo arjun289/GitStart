f28b7e3f2c4357d9a52ddfa0786dab20a049786f
arjun289 <arjun289singh@gmail.com>
2016-09-13 23:55:00 +0530

    hello

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+int main(){
+     
+  int a;	
+  flot b;
+
+  while(1){
+    if()
+      vector<int>a();
+          
+
+  }
+
+}
